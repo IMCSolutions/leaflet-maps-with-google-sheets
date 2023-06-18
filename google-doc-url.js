93c3b516b41eb55b470e36ec2e1011852d76136d
IMCSolutions <136998363+IMCSolutions@users.noreply.github.com>
2023-06-18 20:38:54 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1VbuDxLAnDw36pZpHAnrjI2FbNDzuI1FpNR5XVsSgI5s/edit#gid=1938159830';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1VbuDxLAnDw36pZpHAnrjI2FbNDzuI1FpNR5XVsSgI5s/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyA_fYWX7y7hbn3PE-Rl6eUsEITAzd7aaaM';
